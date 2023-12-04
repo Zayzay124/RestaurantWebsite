c8aefbac81e5dc353d0494924cf48a0d90813b40
Zayzay124 <isaiah.rettig@icloud.com>
2023-12-04 16:26:17 -0500

    the main page

---
+++ Main_Page/main-page/src/image.js
@@ -0,0 +1,12 @@
+import React from "react";
+import img1 from './images/autumn_cafe_entrance.jpg';
+
+function Imagess() {
+    return(
+        <div>
+            <img src={img1} alt=""/>
+        </div>
+    )
+}
+
+export default Imagess;
