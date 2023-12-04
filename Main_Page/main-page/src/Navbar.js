402f15bd03b42458bf99dce4cd4e953fc55d4d7f
Zayzay124 <isaiah.rettig@icloud.com>
2023-12-04 18:20:20 -0500

    updated

--- Main_Page/main-page/src/Navbar.js
+++ Main_Page/main-page/src/Navbar.js
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom"
 
 export default function Navbar() {
-    return <nav className="main_nav">
-        <Link to="/Menu">Menu</Link>
-        <Link to="/Online-Order">Online Ordering</Link>
+    return <nav class="main_nav">
+        <Link to="https://www.google.com/">Menu</Link>
+        <Link to="/Online_Order">Online Ordering</Link>
         <Link to="/Careers">Career</Link>
         <Link to="/Locations">Locations</Link>
         <Link to="/Contact">Contact</Link>
